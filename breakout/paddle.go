2a9d9721f1da5065c89fccd9cd95c933c71d46d9
Björn Ritzl <bjorn.ritzl@king.com>
2017-03-17 07:41:42 +0100

    Cleaned up the assets

--- breakout/paddle.go
+++ breakout/paddle.go
@@ -1,26 +1,6 @@
 components {
   id: "script"
-  component: "/main/ball.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/assets/breakout.atlas\"\n"
-  "default_animation: \"ballGrey_01\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
+  component: "/breakout/paddle.script"
   position {
     x: 0.0
     y: 0.0
@@ -41,13 +21,12 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"ball\"\n"
-  "mask: \"brick\"\n"
+  "group: \"paddle\"\n"
+  "mask: \"ball\"\n"
   "mask: \"wall\"\n"
-  "mask: \"paddle\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_SPHERE\n"
+  "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: 0.0\n"
   "      y: 0.0\n"
@@ -60,13 +39,35 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 1\n"
+  "    count: 3\n"
   "  }\n"
-  "  data: 65.0\n"
+  "  data: 320.0\n"
+  "  data: 70.0\n"
+  "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/breakout.atlas\"\n"
+  "default_animation: \"paddle_02\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
     x: 0.0
